ff8f0fcc6e6ab53a07a7ae99ff414bc5f34adefd
Light Yagami <Alessandro824@proton.me>
2024-11-10 21:13:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,30 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
+// Configurazione della connessione al database
+$servername = "localhost";
+$username = "root";
 $password = "";
 $db_name = "test";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+// Connessione al database con controllo degli errori
+try {
+    // Creazione della connessione
+    $conn = new mysqli($servername, $username, $password, $db_name);
 
-if (!$conn) {
-	echo "Connection failed!";
+    // Verifica della connessione
+    if ($conn->connect_error) {
+        throw new Exception("Connessione fallita: " . $conn->connect_error);
+    }
+
+    // Connessione riuscita
+    echo "Connessione al database riuscita!";
+
+} catch (Exception $e) {
+    // Gestione degli errori
+    echo "Errore di connessione: " . $e->getMessage();
 }
+
+// Chiusura della connessione
+$conn->close();
+
+?>
