f7a61bb2080c300ee9c5cfdd3ff3acb8c97777a8
Light Yagami <Alessandro824@proton.me>
2024-11-10 21:16:23 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,21 @@
 <?php
 
+// Avvia la sessione
 session_start();
+
+// Rimuovi tutte le variabili di sessione
 $_SESSION = array();
+
+// Se si desidera distruggere completamente la sessione, anche il cookie di sessione deve essere cancellato.
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+
+// Distruggi la sessione
 session_destroy();
-header('location:login.php');
+
+// Reindirizza l'utente alla pagina di login
+header('Location: login.php');
+exit();
 
 ?>
